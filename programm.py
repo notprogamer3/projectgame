df7b35da5f0d1054be1e7fb57b43ac82a69c262e
notprogamer3 <77282026+notprogamer3@users.noreply.github.com>
2021-01-15 21:53:38 +0300

    new print

--- programm.py
+++ programm.py
@@ -1,9 +1,53 @@
-def main():
-    print('My first git program')
-    print('And I change it every day')
-    print('Again')
-    print('UFO came and added this line')
+import sys
+import random
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+
+
+class Example(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        self.do_paint = False
+
+    def initUI(self):
+        uic.loadUi('Ui.ui', self)
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            qp.setBrush(QColor('yellow'))
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        a = random.randrange(50, 200)
+        qp.drawEllipse(40, 40, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(400, 40, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(700, 40, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(40, 350, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(40, 600, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(700, 600, a, a)
+        a = random.randrange(50, 200)
+        qp.drawEllipse(400, 600, a, a)
+        self.do_paint = False
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
